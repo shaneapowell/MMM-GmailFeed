ff7570926bdd619e7b26efa00746e61e4514cef3
Shane Powell <shaneapowell@hotmail.com>
2019-12-21 21:16:02 -0600

    Initial re-write of MMM-JsonTable to show gmail inbox

---
+++ MMM-GmailFeed.js
@@ -0,0 +1,165 @@
+'use strict';
+
+Module.register("MMM-GmailFeed", {
+
+	jsonData: null,
+	errorData: null,
+
+	// Default module config.
+	defaults: {
+		url: "",
+		updateInterval: 15000,
+		maxEmails: 5,
+		maxSubjectLength: 40,
+		maxFromLength: 15
+	},
+
+	start: function () {
+		this.getJson();
+		this.scheduleUpdate();
+	},
+
+	scheduleUpdate: function () {
+		var self = this;
+		setInterval(function () {
+			self.getJson();
+		}, this.config.updateInterval);
+	},
+
+	// Define required scripts.
+	getStyles: function () {
+		return ["MMM-GmailFeed.css"];
+	},
+
+	// Define required scripts.
+	getScripts: function() {
+		return ["moment.js"];
+	},
+
+	// Request node_helper to get json from url
+	getJson: function () {
+		this.sendSocketNotification("MMM-GmailFeed_GET_JSON", this.config);
+	},
+
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === "MMM-GmailFeed_JSON_RESULT") {
+			// Only continue if the notification came from the request we made
+			// This way we can load the module more than once
+			if (payload.username === this.config.username) {
+				this.jsonData = payload.data;
+				this.errorData = null;
+				this.updateDom(500);
+			}
+		}
+		if (notification === "MMM-GmailFeed_JSON_ERROR") {
+			if(payload.username === this.config.username) {
+				this.jsonData = null;
+				this.errorData = "Error: [" + payload.error + "]";
+				this.updateDom(500);
+			}
+		}
+	},
+
+	// Override getHeader method.
+	getHeader: function() {
+		if (!this.jsonData) {
+			return "GmailFeed";
+		}
+		return this.jsonData.title + "  -  " + this.jsonData.fullcount;
+	},
+
+	// Override dom generator.
+	getDom: function () {
+
+		var wrapper = document.createElement("table");
+		wrapper.classList.add("mailtable");
+		if (this.errorData) {
+			wrapper.innerHTML = this.errorData;
+			return wrapper;
+		}
+
+		if (!this.jsonData) {
+			wrapper.innerHTML = "Loading...";
+			return wrapper;
+		}
+
+		if (!this.jsonData.entry) {
+			var row = document.createElement("tr");
+			wrapper.append(row);
+
+			var cell = document.createElement("td");
+			row.append(cell);
+			cell.append(document.createTextNode("No New Mail"));
+			cell.setAttribute("colspan", "4");
+			return wrapper;
+		}
+
+		var items = this.jsonData.entry;
+
+		// If the items is null, no new messages
+		if (!items) {
+			return wrapper;
+		}
+	
+		// If the items is not an array, it's a single entry
+		if (!Array.isArray(items)) {
+			items = [ items ]
+		}
+	
+		items.forEach(element => {
+			var row = this.getTableRow(element);
+			wrapper.appendChild(row);
+		});
+
+		return wrapper;
+	},
+
+	getTableRow: function (jsonObject) {
+		var row = document.createElement("tr");
+		row.classList.add("normal");
+
+		var fromNode = document.createElement("td");
+		var subjNode = document.createElement("td");
+		var dtNode = document.createElement("td");
+		var tmNode = document.createElement("td");
+
+		var issueDt = moment(jsonObject.issued);
+
+		fromNode.append(document.createTextNode(jsonObject.author.name.substring(0, this.config.maxFromLength)));
+		subjNode.append(document.createTextNode(jsonObject.title.substring(0, this.config.maxSubjectLength)));
+		if (!issueDt.isSame(new Date(), "day")) {
+			dtNode.append(document.createTextNode(issueDt.format("MMM DD - ")));
+		}
+		tmNode.append(document.createTextNode(issueDt.format("h:mm a")));
+
+		fromNode.classList.add("colfrom");
+//		fromNode.classList.add("light");
+//		fromNode.setAttribute("align", "left");
+//		fromNode.classList.add("small");
+//		fromNode.setAttribute("nowrap", "");
+		subjNode.classList.add("colsubj");
+//		subjNode.setAttribute("align", "left");
+//		subjNode.classList.add("title");
+		dtNode.classList.add("coldt");
+//		dtNode.classList.add("light");
+//		dtNode.classList.add("date");
+//		dtNode.classList.add("small");
+//		dtNode.setAttribute("align", "right");
+//		dtNode.setAttribute("nowrap", "");
+		tmNode.classList.add("coltm");
+//		tmNode.classList.add("light");
+//		tmNode.classList.add("time");
+//		tmNode.classList.add("small");
+//		tmNode.setAttribute("align", "right");
+//		tmNode.setAttribute("nowrap", "");
+//		tmNode.setAttribute("width", "1px");
+
+		row.append(fromNode);
+		row.append(subjNode);
+		row.append(dtNode);
+		row.append(tmNode);
+	
+		return row;
+	}
+
+});
--- node_helper.js
+++ node_helper.js
@@ -1,28 +1,47 @@
 var NodeHelper = require('node_helper');
 var request = require('request');
+var xml2js = require('xml2js');
 
 module.exports = NodeHelper.create({
 	start: function () {
-		console.log('MMM-JsonTable helper started...');
+		console.log('MMM-GmailFeed helper started...');
 	},
 
-	getJson: function (url) {
+	getFeed: function (config) {
 		var self = this;
+		var feedUrl = "https://mail.google.com/mail/feed/atom";
 
-		request({ url: url, method: 'GET' }, function (error, response, body) {
+		request({url: feedUrl, auth: { user: config.username, pass: config.password } } , function (error, response, body) {
 			if (!error && response.statusCode == 200) {
-				var json = JSON.parse(body);
-				// Send the json data back with the url to distinguish it on the receiving part
-				self.sendSocketNotification("MMM-JsonTable_JSON_RESULT", {url: url, data: json});
+				var parser = new xml2js.Parser({trim:true, explicitArray: false });
+				parser.parseString(body, function (err, result) {
+
+				if (result.feed.entry) {
+					if (!Array.isArray(result.feed.entry)) {
+						result.feed.entry = [ result.feed.entry ]
+					}
+					
+					result.feed.entry = result.feed.entry.slice(0, config.maxEmails);
+				}
+
+//console.log("----");
+//console.log(JSON.stringify(result.feed, null, 2));
+//console.log("----");
+					// Send the json data back with teh url to distinguish it on the receiving port
+					self.sendSocketNotification("MMM-GmailFeed_JSON_RESULT", {username: config.username, data: result.feed});
+				});
+			}
+			else {
+				self.sendSocketNotification("MMM-GmailFeed_JSON_ERROR", {username: config.username, error: error });
 			}
 		});
 
 	},
 
 	//Subclass socketNotificationReceived received.
-	socketNotificationReceived: function (notification, url) {
-		if (notification === "MMM-JsonTable_GET_JSON") {
-			this.getJson(url);
+	socketNotificationReceived: function (notification, config) {
+		if (notification === "MMM-GmailFeed_GET_JSON") {
+			this.getFeed(config);
 		}
 	}
 });
